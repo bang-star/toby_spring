1ec652b01a82a6037b19a4f57e6f04ff2c7b4b83
bang-star <bang77060986@gmail.com>
2022-10-23 18:44:37 +0900

    Update  - 수정자 메소드 DI를 사용해 UserDao 타입의 빈을 만드는 DaoFactory의 userDao 메소드

--- vol1/spring_practice_01/src/main/java/com/dao/DaoFactory.java
+++ vol1/spring_practice_01/src/main/java/com/dao/DaoFactory.java
@@ -9,7 +9,8 @@ public class DaoFactory {
 
     @Bean       // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
     public UserDao userDao(){
-        UserDao userDao = new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
         return userDao;
     }
 
