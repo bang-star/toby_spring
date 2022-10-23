a0e8d1683ea2bcd9d4783797332aaa694efee0de
bang-star <bang77060986@gmail.com>
2022-10-23 17:25:15 +0900

    Fix Spelling

--- vol1/spring_practice_01/src/main/java/com/dao/CountingConnectionMaker.java
+++ vol1/spring_practice_01/src/main/java/com/dao/CountingConnectionMaker.java
@@ -4,17 +4,17 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class CountingConnectionMaker implements ConnectionMaker{
-    int counter = 0;
-    private ConnectionMaker realconnectionMaker;
+    private int counter = 0;
+    private ConnectionMaker realConnectionMaker;
 
-    public CountingConnectionMaker(ConnectionMaker realconnectionMaker) {
-        this.realconnectionMaker = realconnectionMaker;
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
     }
 
     @Override
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
         this.counter++;
-        return realconnectionMaker.makeConnection();
+        return realConnectionMaker.makeConnection();
     }
 
     public int getCounter() {
